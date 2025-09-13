76104f86567a6cd78a04a5ec6cd59685729fe8e6
shoelranaovi <shoelranasajjad7877@gamil.com>
2025-09-14 00:36:25 +0600

    add shadcn ui

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 1.Create project with next js ---done
-2.imprement google font in next js project
+2.imprement google font in next js project----done
+3.update mata data ---done
+4.install shadcn ui ---done
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
+
   return (
-  <div>
-    <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-  </div>
+    <div>
+      <h1 className="text-3xl font-bold underline">Whereas disregard and contempt for human rights have resulted</h1>
+      <Button>Click me</Button>
+      
+       
+    </div>
   );
 }
